f84888664c3e7189f32c65d96b33f1447707ed2f
Natthawit Janjaroen <natthawit.jan@gmail.com>
2020-08-04 14:38:36 +0700

    [Modified] Refactored all.

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,81 +1,31 @@
+import java.util.Map;
+
 public class TennisGame2 implements TennisGame {
     private int player2Point;
     private int player1Point;
 
+    private final Map<Integer, String> SCORE_DECODER = Map.of(0, "Love", 1, "Fifteen", 2, "Thirty", 3, "Forty");
 
     public TennisGame2(String player1Name, String player2Name) {
     }
 
     public String getScore() {
-        String score = "";
-        String player1Result = "";
-        String player2Result = "";
 
-
+        // handle Deuce and exactly the same point
         if (player2Point == player1Point && player2Point <= 4)
             return equalPoint(player2Point);
-
-        else if (player2Point > 0 && player1Point == 0) {
-            if (player2Point == 1)
-                player1Result = "Fifteen";
-            if (player2Point == 2)
-                player1Result = "Thirty";
-            if (player2Point == 3)
-                player1Result = "Forty";
-
-            player2Result = "Love";
-            score = player1Result + "-" + player2Result;
+            // No one wins, simply tells score
+        else if (player1Point < 4 && player2Point < 4) {
+            return String.format("%s-%s", SCORE_DECODER.get(player1Point), SCORE_DECODER.get(player2Point));
         }
-        if (player1Point > 0 && player2Point == 0) {
-            if (player1Point == 1)
-                player2Result = "Fifteen";
-            if (player1Point == 2)
-                player2Result = "Thirty";
-            if (player1Point == 3)
-                player2Result = "Forty";
-
-            player1Result = "Love";
-            score = player1Result + "-" + player2Result;
+        // Check if there is a winner
+        else if (Math.abs(player1Point - player2Point) >= 2) {
+            return (player2Point > player1Point) ? "Win for player2" : "Win for player1";
         }
-
-        if (player2Point > player1Point && player2Point < 4) {
-            if (player2Point == 2)
-                player1Result = "Thirty";
-            if (player2Point == 3)
-                player1Result = "Forty";
-            if (player1Point == 1)
-                player2Result = "Fifteen";
-            if (player1Point == 2)
-                player2Result = "Thirty";
-            score = player1Result + "-" + player2Result;
+        // There is no winner, meaning that there was a deuce and a player has the advantage
+        else {
+            return (player2Point > player1Point) ? "Advantage player2" : "Advantage player1";
         }
-        if (player1Point > player2Point && player1Point < 4) {
-            if (player1Point == 2)
-                player2Result = "Thirty";
-            if (player1Point == 3)
-                player2Result = "Forty";
-            if (player2Point == 1)
-                player1Result = "Fifteen";
-            if (player2Point == 2)
-                player1Result = "Thirty";
-            score = player1Result + "-" + player2Result;
-        }
-
-        if (player2Point > player1Point && player1Point >= 3) {
-            score = "Advantage player1";
-        }
-
-        if (player1Point > player2Point && player2Point >= 3) {
-            score = "Advantage player2";
-        }
-
-        if (player2Point >= 4 && player1Point >= 0 && (player2Point - player1Point) >= 2) {
-            score = "Win for player1";
-        }
-        if (player1Point >= 4 && player2Point >= 0 && (player1Point - player2Point) >= 2) {
-            score = "Win for player2";
-        }
-        return score;
     }
 
     private String equalPoint(int p1point) {
@@ -91,11 +41,11 @@ public class TennisGame2 implements TennisGame {
 
 
     public void P1Score() {
-        player2Point++;
+        player1Point++;
     }
 
     public void P2Score() {
-        player1Point++;
+        player2Point++;
     }
 
     public void wonPoint(String player) {
